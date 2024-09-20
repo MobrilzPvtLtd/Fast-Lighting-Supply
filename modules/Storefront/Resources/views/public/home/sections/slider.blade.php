0f0f4f74ac2f968aa6dabdcb5f780d63f5a91d41
PriyankaMobrilz <priyanka@mobrilz.com>
2024-09-20 10:40:52 +0530

    Update slider.blade.php

--- modules/Storefront/Resources/views/public/home/sections/slider.blade.php
+++ modules/Storefront/Resources/views/public/home/sections/slider.blade.php
@@ -13,24 +13,28 @@
                 >
                     @foreach ($slider->slides as $slide)
                         <div class="slide">
-
                             @if ($slider->fade)
-                                <a href="{{ $slide->call_to_action_url }}"
+                            <a href="{{ $slide->call_to_action_url }}">
+                                <img src="{{ $slide->file->path }}" class="slider-image">
+                            </a>
+
+                                {{-- <a href="{{ $slide->call_to_action_url }}"
                                     data-animation-in="{{ data_get($slide->options, 'call_to_action.effect', 'fadeInRight') }}"
                                     data-delay-in="{{ data_get($slide->options, 'call_to_action.delay', '0.7') }}"
                                     target="{{ $slide->open_in_new_window ? '_blank' : '_self' }}">
                                     <img src="{{ $slide->file->path }}" class="slider-image">
-                                </a>
+                                </a> --}}
                             @else
-                                <a href="{{ $slide->call_to_action_url }}"
+                            <img src="{{ $slide->file->path }}" data-animation-in="zoomInImage" class="slider-image animate_animated">
+                                {{-- <a href="{{ $slide->call_to_action_url }}"
                                     data-animation-in="{{ data_get($slide->options, 'call_to_action.effect', 'fadeInRight') }}"
                                     data-delay-in="{{ data_get($slide->options, 'call_to_action.delay', '0.7') }}"
                                     target="{{ $slide->open_in_new_window ? '_blank' : '_self' }}">
                                     <img src="{{ $slide->file->path }}" data-animation-in="zoomInImage" class="slider-image animate_animated">
-                                </a>
+                                </a> --}}
                             @endif
 
-                            {{-- <div class="slide-content {{ $slide->isAlignedLeft() ? 'align-left' : 'align-right' }}">
+                            <div class="slide-content {{ $slide->isAlignedLeft() ? 'align-left' : 'align-right' }}">
                                 <div class="captions">
                                     <span
                                         class="caption caption-1"
@@ -60,7 +64,7 @@
                                         </a>
                                     @endif
                                 </div>
-                            </div> --}}
+                            </div>
                         </div>
                     @endforeach
                 </div>
