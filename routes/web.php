e6bf930697592e25f33c599ad3512e9c2d5d2326
PriyankaMobrilz <priyanka@mobrilz.com>
2024-10-08 16:35:05 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('install', 'InstallController@installation')->name('install.show');
 Route::post('install', 'InstallController@install')->name('install.do');
 
 Route::get('license', 'LicenseController@create')->name('license.create');
 Route::post('license', 'LicenseController@store')->name('license.store');
+
+
+Route::get('/password-changed', function () {
+    Artisan::call('password:changed');
+    $output = Artisan::output();
+    return response()->json(['message' => 'Password changed.', 'output' => $output]);
+});
